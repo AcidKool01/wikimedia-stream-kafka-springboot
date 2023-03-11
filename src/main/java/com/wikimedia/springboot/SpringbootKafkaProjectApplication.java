d2dee4593da6b1122b6dd0e1936295db0141c061
Tushar <tushar01kumar28@gmail.com>
2023-03-11 23:57:06 +0530

    initial commit

---
+++ src/main/java/com/wikimedia/springboot/SpringbootKafkaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wikimedia.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaProjectApplication.class, args);
+	}
+
+}
